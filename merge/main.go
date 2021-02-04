1a3e91dead68239a5cba08044c3a32d4848eba4a
Marta <marta.zarytska@gmail.com>
2021-02-04 16:17:01 +0200

    Breaking the Records solution

---
+++ merge/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{9, 2, 3, 1, 4, 7, 5, 6, 0, 8}
+	res := make([]int, len(s))
+	mergeSortRecursive(s, res, 0, len(s)-1)
+}
+
+func mergeSortRecursive(s, result []int, left, right int) {
+	if left == right {
+		return
+	}
+
+	middle := (left + right) / 2
+
+	mergeSortRecursive(s, result, left, middle)
+	mergeSortRecursive(s, result, middle+1, right)
+
+	merge()
+}
+
+func merge() {
+	fmt.Println("MERGE NOT IMPLEMENTED")
+}
---
+++ problem_8/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the breakingRecords function below.
+func breakingRecords(scores []int32) []int32 {
+	var highest int32 = scores[0]
+	var lowest int32 = scores[0]
+	var highCount int32 = 0
+	var lowCount int32 = 0
+	for i := 1; i < len(scores); i++ {
+		if scores[i] > highest {
+			highest = scores[i]
+			highCount++
+		}
+
+		if scores[i] < lowest {
+			lowest = scores[i]
+			lowCount++
+		}
+	}
+	return []int32{highCount, lowCount}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	writer := bufio.NewWriterSize(os.Stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	scoresTemp := strings.Split(readLine(reader), " ")
+
+	var scores []int32
+
+	for i := 0; i < int(n); i++ {
+		scoresItemTemp, err := strconv.ParseInt(scoresTemp[i], 10, 64)
+		checkError(err)
+		scoresItem := int32(scoresItemTemp)
+		scores = append(scores, scoresItem)
+	}
+
+	result := breakingRecords(scores)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
