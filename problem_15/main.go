537f94836d13e288581756371d5bd9bee88d96c9
Marta <marta.zarytska@gmail.com>
2021-03-31 19:30:36 +0300

    add problem_15

---
+++ problem_15/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the miniMaxSum function below.
+func miniMaxSum(arr []int32) {
+
+	var sum int64 = 0
+	var max int64 = 0
+	var min = int64(arr[0])
+	for _, v := range arr {
+		value := int64(v)
+		if value > max {
+			max = value
+		}
+
+		if value < min {
+			min = value
+		}
+
+		sum += value
+
+	}
+
+	fmt.Println(sum-max, sum-min)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	arrTemp := strings.Split(readLine(reader), " ")
+
+	var arr []int32
+
+	for i := 0; i < 5; i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
