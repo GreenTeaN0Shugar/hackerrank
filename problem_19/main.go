8e0ebe06b739fd3c72139371b8ac4798acaff0be
Marta <marta.zarytska@gmail.com>
2021-04-06 18:27:47 +0300

    add problem_19

---
+++ problem_19/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var alphabet = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+// Complete the designerPdfViewer function below.
+func designerPdfViewer(h []int32, word string) int32 {
+	var maxHight int32 = 0
+
+	for _, r := range word {
+		for i, v := range alphabet {
+			wordLetter := string(r)
+			if strings.EqualFold(wordLetter, v) {
+				if h[i] > maxHight {
+					maxHight = h[i]
+				}
+			}
+		}
+	}
+
+	return maxHight * int32(len(word))
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	hTemp := strings.Split(readLine(reader), " ")
+
+	var h []int32
+
+	for i := 0; i < 26; i++ {
+		hItemTemp, err := strconv.ParseInt(hTemp[i], 10, 64)
+		checkError(err)
+		hItem := int32(hItemTemp)
+		h = append(h, hItem)
+	}
+
+	word := readLine(reader)
+
+	result := designerPdfViewer(h, word)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
